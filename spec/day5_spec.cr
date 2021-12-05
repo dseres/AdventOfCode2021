ce073fae7f8c29a2a0d01d1f3f5f082ef62d718e
David Seres <dseres001@gmail.com>
2021-12-05 16:01:31 +0100

    halfway of day5

--- spec/day5_spec.cr
+++ spec/day5_spec.cr
@@ -2,9 +2,31 @@ require "./spec_helper"
 
 describe AdventOfCode2021 do
   it "day5 should work" do
-    str = ""
+    str = "0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2"
+    output_matrix = ".......1..
+..1....1..
+..1....1..
+.......1..
+.112111211
+..........
+..........
+..........
+..........
+222111...."
     input = AdventOfCode2021::Day5.parse_input(str)
-    AdventOfCode2021::Day5.solution1(input).should eq(0)
+    matrix = AdventOfCode2021::Day5.create_matrix(input)
+    puts matrix
+    AdventOfCode2021::Day5.print_matrix(matrix).should eq(output_matrix)
+    AdventOfCode2021::Day5.solution1(input).should eq(5)
     AdventOfCode2021::Day5.solution2(input).should eq(0)
   end
 end
--- src/day5/day5.cr
+++ src/day5/day5.cr
@@ -4,8 +4,36 @@ module AdventOfCode2021
 
     NUMBER = 5
 
-    def parse_input(input : String) : Array(String)
-      input.lines
+    def parse_input(input : String) 
+      coordinates = Array(Array(Int32)).new
+      input.each_line do |line|
+        if /^(\d+),(\d+) -> (\d+),(\d+)$/ =~ line
+          coordinates << $~[1..4].map &.to_i32
+        end
+      end
+      p! coordinates
+      coordinates
+    end
+
+    def create_matrix(coordinates)
+      max_x = coordinates.max_of { |row| Math.max(row[0], row[2])}
+      max_y = coordinates.max_of { |row| Math.max(row[1], row[3])}
+      matrix = Array(Array(Int32)).new
+      (1..max_x+1).each do
+        matrix<<Array(Int32).new(max_y + 1, 0)
+      end
+      coordinates.each do |vent|
+        (vent[0]..vent[2]).each do |x|
+          (vent[1]..vent[3]).each do |y|
+            matrix[x][y] += 1
+          end
+        end
+      end
+      matrix
+    end
+
+    def print_matrix(matrix)
+      matrix.join("\n"){ |row| row.map{|i| i == 0? "." : i.to_s}.join("")}
     end
 
     def solution1(input) : Int32
