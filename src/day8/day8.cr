2243f247f156a308633f22fa515d3e3c6653102a
David Seres <dseres001@gmail.com>
2021-12-08 13:51:54 +0100

    refactoring of day8

--- src/day8/day8.cr
+++ src/day8/day8.cr
@@ -25,37 +25,46 @@ module AdventOfCode2021
     def solution2(input : Array(Array(String))) : Int32
       input.sum do |signals|
         # p! signals[0..9].map{ |signal| signal.size }.sort
-        chars = solve_signals signals
+        chars = solve signals
         get_number signals, chars
       end
     end
 
-    private def solve_signals(signals)
+    private def solve(signals) : Hash(Char, Char)
       # p! signals
-      chars = Hash(String | Char, Array(Char)).new # chars of a display, e.g. abcdefg
+      chars = Hash( (String | Char), Array(Char)).new # chars of a display, e.g. abcdefg
+      compute_segment_groups signals, chars
+      compute_segments signals, chars
+      chars.select { |c, _v| c.is_a? Char }.map { |c, chars| Tuple.new(chars[0].as(Char), c.as(Char)) }.to_h
+    end
+
+    private def compute_segment_groups(signals, chars)
       # Number 1 with 2 segments  c,f
       signals.select { |s| s.size == 2 }.each do |s|
         chars["cf"] = s.chars
       end
-      # Number 0,6,8 with 9 segments -> a,b,f,g
-      chars["abfg"] = signals.select { |s| s.size == 6 }.map { |s| s.chars }.reduce { |intersect, signal| intersect & signal }
-      # We have 'c','f'
-      chars['f'] = chars["cf"] & chars["abfg"]
-      chars['c'] = chars["cf"] - chars['f']
       # Number 7 with 3 segments, a,c,f
       signals.select { |s| s.size == 3 }.each do |s|
-        # we have the 'a'
         chars["acf"] = s.chars
-        chars['a'] = chars["acf"] - chars["cf"]
       end
+      # Number 0,6,8 with 6 segments -> a,b,f,g
+      chars["abfg"] = signals.select(&.size.== 6).map(&.chars).reduce { |chars1, chars2| chars1 & chars2 }
       # Number 4 with 4 segments, b,c,d,f
       signals.select { |s| s.size == 4 }.each do |s|
         # two possible value for 'b','d'
         chars["bcdf"] = s.chars
-        chars["bd"] = chars["bcdf"] - chars["cf"]
       end
       # Number 2,3,5 with 5 segments -> a,d,g
-      chars["adg"] = signals.select { |s| s.size == 5 }.map { |s| s.chars }.reduce { |intersect, signal| intersect & signal }
+      chars["adg"] = signals.select(&.size.== 5).map(&.chars).reduce { |chars1, chars2| chars1 & chars2 }
+    end
+
+    private def compute_segments(signals, chars)
+      # We have 'c','f'
+      chars['f'] = chars["cf"] & chars["abfg"]
+      chars['c'] = chars["cf"] - chars['f']
+      # we have the 'a'
+      chars['a'] = chars["acf"] - chars["cf"]
+      chars["bd"] = chars["bcdf"] - chars["cf"]
       chars['d'] = chars["bd"] & chars["adg"]
       chars['b'] = chars["bd"] - chars['d']
       # we have 'e'
@@ -63,21 +72,23 @@ module AdventOfCode2021
       # we have 'g'
       chars['g'] = "abcdefg".chars - chars['a'] - chars['b'] - chars['c'] - chars['d'] - chars['e'] - chars['f']
       # p! chars
-      chars.select { |c, _v| c.is_a? Char }.map { |c, chars| Tuple.new(chars[0].as(Char), c.as(Char)) }.to_h
     end
 
     private def get_number(signals, chars) : Int32
-      numbers = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
       number = 0
       signals[10..13].each do |s|
-        # p! chars, numbers, s
-        encoded = s.chars.map { |c| chars[c] }.sort.join
-        # p! encoded
-        digit = numbers.index(encoded).not_nil!
-        # p! digit
+        digit = get_digit s, chars
         number = number * 10 + digit
       end
       number
+    end
+
+    # Numbers' representation from 0..9
+    @@numbers = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+
+    private def get_digit(s, chars)
+      encoded = s.chars.map { |c| chars[c] }.sort.join
+      @@numbers.index(encoded).not_nil!
     end
 
     def main
