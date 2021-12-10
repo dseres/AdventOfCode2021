ca213cbea6c47fbd524c036ae8cb30b14c316efb
David Seres <dseres001@gmail.com>
2021-12-10 13:42:49 +0100

    day10 solved

--- spec/day10_spec.cr
+++ spec/day10_spec.cr
@@ -2,9 +2,20 @@ require "./spec_helper"
 
 describe AdventOfCode2021 do
   it "day10 should work" do
-    str = ""
+    str = <<-INPUT
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+INPUT
     input = AdventOfCode2021::Day10.parse_input(str)
-    AdventOfCode2021::Day10.solution1(input).should eq(0)
-    AdventOfCode2021::Day10.solution2(input).should eq(0)
+    AdventOfCode2021::Day10.solution1(input).should eq(26397)
+    AdventOfCode2021::Day10.solution2(input).should eq(288957)
   end
 end
--- src/day10/day10.cr
+++ src/day10/day10.cr
@@ -2,23 +2,76 @@ module AdventOfCode2021
   module Day10
     extend self
 
-    NUMBER = 10
+    DAY = 10
+
+    @@points = {
+      '(' => 1,
+      '[' => 2,
+      '{' => 3,
+      '<' => 4,
+
+      ')' => 3,
+      ']' => 57,
+      '}' => 1197,
+      '>' => 25137,
+    }
+
+    @@pairs = {
+      ')' => '(',
+      ']' => '[',
+      '}' => '{',
+      '>' => '<',
+    }
 
     def parse_input(input : String) : Array(String)
       input.lines
     end
 
-    def solution1(input) : Int32
-      0
+    def solution1(input : Array(String)) : Int32
+      input.sum do |line|
+        result = parse_line line
+        result.is_a?(Char) ? @@points[result.as(Char)] : 0
+      end
     end
 
-    def solution2(input) : Int32
-      0
+    private def parse_line(line) : Array(Char) | Char
+      stack = [] of Char
+      line.chars.each do |c|
+        case c
+        when ')', ']', '}', '>'
+          pair = stack.pop
+          if pair != @@pairs[c]
+            return c
+          end
+        when '(', '[', '{', '<'
+          stack.push c
+        end
+      end
+      stack
+    end
+
+    def solution2(input : Array(String)) : Int64
+      scores = input
+        .map { |line| parse_line line }
+        .select(&.is_a?(Array(Char)))
+        .map(&.as(Array(Char)))
+        .map { |stack| get_stack_points stack, 0 }
+        .sort
+      scores[scores.size // 2]
+    end
+
+    private def get_stack_points(stack, initial : Int64) : Int64
+      if stack.empty?
+        initial
+      else
+        next_value = initial * 5 + @@points[stack.pop] 
+        get_stack_points stack, next_value
+      end
     end
 
     def main
-      input = parse_input File.read "./src/day#{NUMBER}/input.txt"
-      puts "Solutions of day#{NUMBER} : #{solution1 input} #{solution2 input}"
+      input = parse_input File.read "./src/day#{DAY}/input.txt"
+      puts "Solutions of day#{DAY} : #{solution1 input} #{solution2 input}"
     end
   end
 end
