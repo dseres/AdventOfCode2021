1dbc5dab70380f789c7308c6cd31c5680dc3ee2a
David Seres <dseres001@gmail.com>
2021-12-19 16:12:14 +0100

    reduce started working

--- spec/day18_spec.cr
+++ spec/day18_spec.cr
@@ -5,7 +5,7 @@ alias SailfishNumber=AdventOfCode2021::Day18::SailfishNumber
 describe AdventOfCode2021 do
 
 
-  it "parsing of sailfish numbers as a tree structure should give the input", focus: true do
+  it "parsing of sailfish numbers as a tree structure should give the input" do
     input1 = "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"
     sn1 = SailfishNumber.new input1
     sn1.to_s.should eq(input1)
@@ -15,12 +15,35 @@ describe AdventOfCode2021 do
     sn2.to_s.should eq(input2)
   end
 
-  it "adding sailfish numbers should give the reduced result", focus: true do
+  it "two sailfish number equals if there are the same numbers in them"  do
+    inputs = ["[1, 2]", "[[3, 4], 5]", "[[1, 2], [[3, 4], 5]]", "[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]"]
+    inputs.each do |str|
+      SailfishNumber.new(str).should eq(SailfishNumber.new(str))
+    end
+  end
+
+  it "adding sailfish numbers should give the reduced result" do
     sn1 = SailfishNumber.new "[1, 2]"
     sn2 = SailfishNumber.new "[[3, 4], 5]"
     sn3 = SailfishNumber.new "[[1, 2], [[3, 4], 5]]"
     (sn1 + sn2).should eq(sn3)
+
   end
+
+  it "exploding pairs (reducing) of SailfishNumber examples should work", focus: true do
+    sn = SailfishNumber.new("[[[[[9,8],1],2],3],4]")
+    sn.reduce.should eq (SailfishNumber.new("[[[[0,9],2],3],4]"))
+
+    sn = SailfishNumber.new("[7,[6,[5,[4,[3,2]]]]]")
+    sn.reduce.should eq (SailfishNumber.new("[7,[6,[5,[7,0]]]]"))
+
+    sn = SailfishNumber.new("[[6,[5,[4,[3,2]]]],1]")
+    sn.reduce.should eq (SailfishNumber.new("[[6,[5,[7,0]]],3]"))
+
+    sn = SailfishNumber.new("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")
+    sn.reduce.should eq (SailfishNumber.new("[[3,[2,[8,0]]],[9,[5,[7,0]]]]"))  
+  end
+  
 
   it "day18 should work" do
     # alias SailfishNumber = SailfishNumber
--- src/day18/day18.cr
+++ src/day18/day18.cr
@@ -20,6 +20,7 @@ module AdventOfCode2021
       end
 
       def initialize(str : String)
+        # !pp str
         any = JSON.parse str # parse as json
         build_tree any
       end
@@ -35,11 +36,13 @@ module AdventOfCode2021
           @left = any[0].as_i
         else
           @left = SailfishNumber.new(any[0])
+          @left.as(SailfishNumber).parent = self
         end
         if !any[1].as_a?
           @right = any[1].as_i
         else
           @right = SailfishNumber.new(any[1])
+          @right.as(SailfishNumber).parent = self
         end
       end
 
@@ -47,30 +50,121 @@ module AdventOfCode2021
         io << "[" << left << "," << right << "]"
       end
 
-      def ==(other : SailfishNumber) : Bool
-        @left == other.left && @right == other.right
-      end
-
-      def ==(other) : Bool
-        false
-      end
-
       def clone : SailfishNumber
-        n = SailfishNumber.new @left, @right
+        n = SailfishNumber.new @left, @right, @parent
         if @left.is_a? SailfishNumber
           n.left = @left.clone
+          n.left.as(SailfishNumber).parent = n
         end
         if @right.is_a? SailfishNumber
           n.right = @right.clone
+          n.right.as(SailfishNumber).parent = n
         end
         n
+      end
+
+      def ==(other : SailfishNumber)
+        parent_check = parent.nil? && other.parent.nil? || !parent.nil? && !other.parent.nil?
+        parent_check && @left == other.left && @right == other.right
+      end
+
+      def ==(other)
+        false
       end
 
       def +(other : SailfishNumber) : SailfishNumber
         sum = SailfishNumber.new self.clone, other.clone
         sum.left.as(SailfishNumber).parent = sum
         sum.right.as(SailfishNumber).parent = sum
+        # sum.reduce
         sum
+      end
+
+      def reduce : SailfishNumber
+        while reduce_by_depth || reduce_by_numbers
+        end
+        self
+      end
+
+      def reduce_by_depth : Bool
+        ns2r = find_nodes_to_reduce 1
+        if !ns2r.empty?
+          sorted = [] of self
+          sort_nodes_from_left_to_right(self, sorted)
+          ns2r.each { |node| reduce_node(node, sorted) }
+        end
+        !ns2r.empty?
+      end
+
+      def find_nodes_to_reduce(depth : Int32) : Array(SailfishNumber)
+        nodes = [] of SailfishNumber
+        if depth <= 4
+          if @left.is_a? SailfishNumber
+            nodes += @left.as(SailfishNumber).find_nodes_to_reduce(depth + 1)
+          end
+          if @right.is_a? SailfishNumber
+            nodes += @right.as(SailfishNumber).find_nodes_to_reduce(depth + 1)
+          end
+        else
+          nodes << self
+        end
+        nodes
+      end
+
+      private def sort_nodes_from_left_to_right(node : SailfishNumber, nodes : Array(SailfishNumber))
+        if node.left.is_a? SailfishNumber
+          sort_nodes_from_left_to_right(node.left.as(self), nodes)
+        end
+        if node.left.is_a?(Int32) || node.right.is_a?(Int32)
+          nodes << node
+        end
+        if node.right.is_a? SailfishNumber
+          sort_nodes_from_left_to_right(node.right.as(self), nodes)
+        end
+      end
+
+      private def reduce_node(node : SailfishNumber, nodes : Array(SailfishNumber))
+        index = nodes.index { |n| n.object_id == node.object_id }.not_nil!
+        lno = node.left.as(Int32)
+        rno = node.right.as(Int32)
+        add_left_to_the_previous(lno, index, nodes)
+        add_right_to_the_next(rno, index, nodes)
+        replace_with_zero node
+      end
+
+      private def add_left_to_the_previous(value : Int32, index : Int32, nodes : Array(SailfishNumber))
+        if index > 0
+          if nodes[index - 1].right.is_a? self
+            nodes[index - 1].left = nodes[index - 1].left.as(Int32) + value
+          else
+            nodes[index - 1].right = nodes[index - 1].right.as(Int32) + value
+          end
+        end
+      end
+
+      private def add_right_to_the_next(value : Int32, index : Int32, nodes : Array(SailfishNumber))
+        if index < nodes.size - 1
+          if nodes[index + 1].left.is_a? self
+            nodes[index + 1].right = nodes[index + 1].right.as(Int32) + value
+          else
+            nodes[index + 1].left = nodes[index + 1].left.as(Int32) + value
+          end
+        end
+      end
+
+      private def replace_with_zero(node : SailfishNumber)
+        parent = node.parent
+        if !parent.nil?
+          if parent.left == node
+            parent.left = 0
+          elsif parent.right == node
+            parent.right = 0
+          end
+        end
+      end
+
+      private def reduce_by_numbers
+        false
       end
     end
 
