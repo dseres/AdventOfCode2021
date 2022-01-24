eebc5a62b4a91e82166b039b5d33a1c614c7ccb6
dseres <dseres001@gmail.com>
2022-01-24 13:37:24 +0000

    saving work

--- src/day21/quantum_game.cr
+++ src/day21/quantum_game.cr
@@ -3,6 +3,108 @@ require "./*"
 module AdventOfCode2021::Day21
   class QuantumDiracDiceGame
     MAX_POINTS = 21
+    MAX_ROLL = 9
+    POSITIONS = 10
+
+    struct Player
+      getter position : Int8
+      getter score = 0_i8;
+
+      def initialize(@position); end
+
+      def to_s(io : IO)
+        io.print "Player[ position : #{position}, score : #{score}, wins : #{wins}] "
+      end
+
+      def do_turn(roll : Int32)
+        @position += roll
+        while @position > 10
+          @position -= 10
+        end
+        @score += @position
+      end
+
+      def wins?
+        @score >= MAX_POINTS
+      end
+    end
+
+    struct Universe
+      getter current_player = 0_i8
+      getter player1 : Player
+      getter player2 : Player
+      getter counter = 1_i64
+
+      def initialize()
+        @current_player = -1_i8
+        @player1 = Player(0_i8)
+        @player2 = Player(0_i8)
+       end
+
+      def initialize(@player1, @player2); end
+
+      # dice combination will show the sum of 3 rolls how many times equal
+      # see tally function in crystal documentation
+      # the array will be {3 => 1, 4  => 3, 5 => 6, 6 => 7, 7 => 6, 8 => 3, 9 => 1}
+      # which means 5 can be rolled out with 3 dice 6 ways
+      @@roll_prevalence : Hash(Int32,Int32) =  [1, 2, 3].cartesian_product({1, 2, 3}, {1, 2, 3}).map(&.sum).tally
+
+      def is_valid?
+        current_player != -1
+      end
+
+      def is_finished?
+        player1.score >= MAX_POINTS || player2.score >= MAX_POINTS
+      end
+
+      def do_turn(roll, count)
+        if current_player == 0
+          player1.do_turn roll
+          current_player = 1
+        else 
+          player2.do_turn roll
+          current_player = 0
+        end
+        counter *= count
+      end
+    end
+
+    # Array stores all possible universes for the current user
+    @universes = Array(Array(Universe))(POSITIONS+1) do 
+            Array(Universe)))(MAX_POINTS + MAX_ROLLS + 1){Universe.new}
+          end
+        end
+    end
+
+    # # Array stores all possible universes index by current player, position1, score1, position2, score2
+    # @universes = Array(Array(Array(Array(Array(Universe))))).new(2) do 
+    #   Array(Array(Array(Array(Universe))))(POSITIONS+1) do 
+    #     Array(Array(Array(Universe)))(MAX_POINTS + MAX_ROLLS + 1) do 
+    #       Array(Array(Universe))(POSITIONS+1) do 
+    #         Array(Universe)))(MAX_POINTS + MAX_ROLLS + 1){Universe.new}
+    #       end
+    #     end
+    #   end
+    # end
+
+    def do_turn(uni : Universe) 
+      @@roll_prevalence.each do |roll,count|
+        uni2 = uni
+        uni2.do_turn roll,count
+        universes[uni2.current_player][uni2.player1.position][uni2.player1.score][uni2.player2.position][uni2.player2.score] = uni2
+      end
+      universes[uni.current_player][uni.player1.position][uni.player1.score][uni.player2.position][uni.player2.score] = Universe.new
+    end
+    
+    private def parse_input(str) : Universe
+      str.lines[0] =~ /^Player 1 starting position: (\d+)$/
+      player1 = Player.new($~[1].to_i32)
+      str.lines[1] =~ /^Player 2 starting position: (\d+)$/
+      player2 = Player.new($~[1].to_i32)
+      Universe.new(player1, player2)
+    end
+
+
 
     class UniverseStatus
       # key fields
